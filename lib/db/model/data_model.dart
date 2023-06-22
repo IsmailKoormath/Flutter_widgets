bc68d5feee3387a3c39e4fec8922538e32c44b6f
Ismail K <ismailkoormath037@gmail.com>
2023-06-22 13:53:58 +0530

    add model file

---
+++ lib/db/model/data_model.dart
@@ -0,0 +1,13 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 1)
+class StudentModel {
+  
+  @HiveField(0)
+  final String name;
+
+  @HiveField(1)
+  final String age;
+  
+  StudentModel({ required this.name, required this.age});
+}
